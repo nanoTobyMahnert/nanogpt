103e6119d1a7c292ca5a3aea9b797003b9de47e5
saibottrenahm <tobiasmahnert@web.de>
2023-07-23 18:57:55 +1000

    Hello worls changes

--- main.py
+++ main.py
@@ -6,29 +6,9 @@ from quart import request
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
-
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
-
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
-
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.get("/hello/<string:name>")
+async def plugin_hello_name(name):
+    return quart.Response(response=f"Hello {name}!", mimetype="text/plain", status=200)
 
 @app.get("/logo.png")
 async def plugin_logo():
